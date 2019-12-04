11cc08b4bf1bc6b2353a0de1d1007812e56e99dc
Caro <bianco.carolinag@gmail.com>
2019-12-04 10:09:06 -0300

    Mi aplicacion primer commit

---
+++ prueba.py
@@ -0,0 +1,13 @@
+def aguante_el_helado(gustos):
+   print ("A mi me gusta el  " + gustos)
+
+
+gustos_de_helado = ["Dulce granizado", "Chocolate con alemndras", "Menta granizada"]
+
+for gustos in gustos_de_helado:
+    aguante_el_helado(gustos)
+
+for i in range (1, 6):
+    print(i)
+    
+    
